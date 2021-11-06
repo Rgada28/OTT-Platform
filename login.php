2465eda4d5ca3a2a97cc7e5db5e5122420e89c86
Raj Nitin Gada <Rgada28@gmail.com>
2021-11-06 20:14:56 +0530

    changed Sign up and Login page design

--- login.php
+++ login.php
@@ -28,39 +28,42 @@ if ($conn->connect_error) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style1.css">
     <!---we had linked our css file----->
 </head>
+<!-- <style>
+    *{
+        border: 2px solid blue;
+    }
+</style> -->
+
 <body>
     <div class="full-page">
- <div class="navbar">
-           <nav>
-                <ul id='MenuItems'>
-                    <form method="get" action="register.php">
-<li><button class='button3' onclick= "document.getElementById('login-page').style.display='block'" style="width:auto;">SIgn-up</button></li>
-</form>
-                </ul>
-            </nav>
-        </div>
-        <div id='login-form'class='login-page'>
+        <div id='login-form' class='login-page'>
             <div class="form-box">
                 <div class='button-box'>
                     <div id='btn'></div>
                 </div>
-<center><h1 style="color:white;">Login</h1></center>
+                <center>
+                    <h1 style="color:white;">Login</h1>
+                </center>
                 <form id='login' class='input-group-login' method="post">
 
-                    <input type='text' name="username" class='input-field' placeholder='Username' required >
-		    <input type='password' name="password" id="pass" class='input-field'placeholder='Enter Password' required>
-		    <input type='checkbox'class='check-box'><span>Remember Password</span>
-		    <button type='submit' class='submit-btn' name="submit" value="submit">Log in</button>
-		 </form>
-		 
+                    <input type='text' name="username" class='input-field'  placeholder='Username'  required>
+                    <input type='password' name="password" id="pass" class='input-field' placeholder='Enter Password' required>
+                    <input type='checkbox' class='check-box'><label style="color: white;">Remember Password</label>
+                    <button type='submit' class='submit-btn' name="submit" value="submit">Log in</button>
+                    <br><label style="color: white;">New to Flakes? </label><a href="register.php" ; style="color:white" ;>Register</a>
+                    <div>&nbsp;</div>
+                </form>
+              
+
             </div>
         </div>
- <?php 
+        <?php 
 
     if(isset($_POST['submit']) && $_POST['submit']=="submit")
         registration();
@@ -69,5 +72,5 @@ if ($conn->connect_error) {
 
 
 </body>
-</html>
 
+</html>
--- register.php
+++ register.php
@@ -31,42 +31,39 @@ $conn->close();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Netflix</title>
+    <title>Sign up</title>
     <link rel="stylesheet" href="style1.css">
     <!---we had linked our css file----->
 </head>
+
 <body>
     <div class="full-page">
-        <div class="navbar">
-           <nav>
-                <ul id='MenuItems'>
-                    <form method="get" action="login.php">
-<li><button class='button3' onclick= "document.getElementById('login-page').style.display='block'" style="width:auto;">SIgn-in</button></li>
-</form>
 
-                    
-                </ul>
-            </nav>
-        </div>
-        <div id='login-form'class='login-page'>
-            <div class="form-box">
+        <div id='login-form' class='login-page'>
+            <div class="form-box" style="top:10px">
                 <div class='button-box'>
                     <div id='btn'></div>
-                    
+
                 </div>
-		<center><h1 style="color:white;">Registration</h1></center>
-                
-		 <form id='register' class='input-group-register' method="post" action="">
-		     <input type='text' name="username" class='input-field'placeholder='User Name'   required>
-		     <input type='email' name="email" class='input-field'placeholder='Email Id'  required>
-		     <input type='password'  name="password" id="pass" class='input-field'placeholder='Enter Password'  required>
-		     <input type='password' class='input-field'placeholder='Confirm Password'  required>
-		     <input type='checkbox'class='check-box'><span>I agree to the terms and  conditions</span>
+                <center>
+                    <h1 style="color:white;">Sign Up</h1>
+                </center>
+
+                <form id='register' class='input-group-register' method="post" action="">
+                    <input type='text' name="username" class='input-field' placeholder='User Name' required>
+                    <input type='email' name="email" class='input-field' placeholder='Email Id' required>
+                    <input type='password' name="password" id="pass" class='input-field' placeholder='Enter Password' required>
+                    <input type='password' class='input-field' placeholder='Confirm Password' required>
+                    <input type='checkbox' class='check-box'><label>I agree the terms and conditions</label>
+                    <br><label>Already have an account </label><a href="login.php" ; style="color:white">Log in</a>
+                    <div>&nbsp;</div>
                     <button type='submit' class='submit-btn' name="submit" value="submit">Register</button>
-  	
-	         </form>
+
+                </form>
+
 
             </div>
         </div>
@@ -76,6 +73,7 @@ $conn->close();
     if(isset($_POST['submit']) && $_POST['submit']=="submit")
         register();
   ?>
-	
+
 </body>
+
 </html>
