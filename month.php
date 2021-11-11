e72fe5c5a6376b98351f21b36b1e8d110e3dd98c
sachin wankhade <sachinwankhade1998@gmail.com>
2021-11-11 18:42:09 +0530

    dashboard

--- month.php
+++ month.php
@@ -1,25 +1,68 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-
-<head lang="en">
-  <title>Monthly Report</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
+<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="reports.css">
 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
+    .row.content {height: 1500px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height: auto;} 
+    }
+  </style>
 </head>
-
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand mx-auto " href="#">Monthly Report</a>
-    </div>
-  </nav>
 
-  </div>
-  <center>
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-3 sidenav">
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Admin Pannel</a></li>
+        <li><a href="month.php">Monthly Report</a></li>
+        <li><a href="year.php">Yearly Report</a></li>
+        <li><a href="pi.php">subscricer per month</a></li>
+      </ul><br>
+      <div class="input-group">
+        <span class="input-group-btn">
+         
+          
+          </button>
+        </span>
+      </div>
+    </div>
+
+    <div class="col-sm-9">
+       <center>
     <?php
     $servername = "localhost";
     $username = "root";
@@ -32,8 +75,7 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     } else {
-      echo "<br>
-      <br>";
+      echo "<h3>monthly data</h3>";
     }
 
     $sql = "SELECT month, SUM(amount) FROM payment GROUP BY month";
@@ -67,11 +109,17 @@
   </center>
 
   <center>
-    <br>
-    <br>
-    <h3><a href="Admin-home.php" class="btn btn-danger">Back to Admin Home</a></h3>
+    <h3><a href="Admin-home.php" style="text-decoration: none">Back to Admin Section</a></h3>
   </center>
+   
+              <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 </body>
-
 </html>
--- year.php
+++ year.php
@@ -1,24 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-
-<head lang="en">
-  <title></title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
+<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="reports.css">
 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
+    .row.content {height: 1500px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height: auto;} 
+    }
+  </style>
 </head>
-
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand mx-auto " href="#">Yearly Report</a>
+
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-3 sidenav">
+    
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Admin Pannel</a></li>
+        <li><a href="month.php">Monthly Report</a></li>
+        <li><a href="year.php">Yearly Report</a></li>
+        <li><a href="pi.php">subscricer per month</a></li>
+      </ul><br>
+      <div class="input-group">
+        <
+        <span class="input-group-btn">
+          
+        </span>
+      </div>
     </div>
-  </nav>
-  <center>
-    <?php
+
+    <div class="col-sm-9">
+       <center>
+      <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -28,45 +73,53 @@
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    } else {
-      echo "<br>
-      <br>";
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    else{
+      echo "<h3>yearly data</h3>";
     }
 
-    $sql = "SELECT year, SUM(amount) FROM payment GROUP BY year";
+  $sql = "SELECT year, SUM(amount) FROM payment GROUP BY year";
     $result = $conn->query($sql);
 
+  
 
-
-    echo "<table>
+echo"<table>
     <tr>
               <td width= 50%>year</td>
               <td width=50%>sales amount</td> 
-            </tr>";
+            </tr>";          
+        
+        echo "</table>";
 
-    echo "</table>";
 
+  while($row = $result->fetch_assoc()){
+        // output data of each row
 
-    while ($row = $result->fetch_assoc()) {
-      // output data of each row
-
-      echo "<table>
+  echo"<table>
     <tr>
-              <td width= 50%>" . $row["year"] . "</td>
-              <td width=50%>" . $row['SUM(amount)'] . "</td> 
-            </tr>";
-
-      echo "</table>";
-    }
+              <td width= 50%>" .$row["year"]. "</td>
+              <td width=50%>" .$row['SUM(amount)']. "</td> 
+            </tr>";          
+        
+        echo "</table>";
+    } 
 
     $conn->close();
-    ?>
-    <br>
-    <br>
-    <a href="Admin-home.php" class="btn btn-danger">Back to Admin Home</a>
+    ?> 
+  </center>
   </center>
 
-</body>
+ 
+   
+              <br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
+</body>
 </html>
